d697968c9b55249f7d7fa388194707cfb8298863
unknown <bejolo018@gmail.com>
2019-05-15 17:01:04 -0500

    made some file tweaks, begun on components

---
+++ friends/src/Components/FriendsForm.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+class FriendForm extends React.Component {
+
+    state = {
+        friend: {
+            name: '',
+            age: '',
+            email: ''
+        }
+    }
+
+
+
+
+}
